c6931f06460b66a6acd4c80b56ccce0b2fbc0731
Steven Lu <tacticalazn@gmail.com>
2015-12-21 15:37:45 -0500

    solution

--- js/countdown.js
+++ js/countdown.js
@@ -1,3 +1,9 @@
-MyApp.controller('countdown', function($scope) {
+MyApp.controller('countdown', function($scope, $interval) {
+
+  $scope.countdown = 100
+
+  $interval(function() {
+    $scope.countdown--;
+  }, 1000)
 
 })
